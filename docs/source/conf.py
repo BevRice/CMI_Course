4c797f6c9fa11cf9de44c9a0d5b9af9c1848e8c8
BevRice <beverlyjane.rice@gmail.com>
2025-02-19 23:10:32 -1000

    Building

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,33 +19,15 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.mathjax',
     'nbsphinx',
-    'sphinx_rtd_theme',
-    'sphinx_thebe'
+    'sphinx_rtd_theme'
 ]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
 html_theme = "sphinx_rtd_theme"
 
-#nbsphinx_execute = 'always'  # Forces execution of notebooks, generating output
-#nbsphinx_allow_errors = True  # Allows the site to build even with minor errors
-#nbsphinx_output_prompt = 'hidden'  # Hides input/output prompts to clean the display
-
 nbsphinx_allow_errors = True  # Allow notebook conversion even with minor errors
 nbsphinx_execute = 'always'   # Execute notebooks and generate outputs
 html_static_path = ['_static']  # Ensures Sphinx copies static files
 
-
-thebe_config = {
-    "use_thebe": True,
-    "selector": "div.cell",  # Matches Jupyter cells
-    "repository_url": "https://github.com/bmgag/CMI_Course",  # Update with your GitHub repo
-    "branch": "main",  # Change to the correct branch
-}
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-#html_theme = 'alabaster'
-#html_static_path = ['_static']
 
